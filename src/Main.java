81fc9d947aa7b273fd2a551d1f537e78bb3aae61
Alexslesa <San4o192-1@mail.ru>
2025-12-30 00:30:04 +0300

    instal commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+/**
+ * @autor Alexandr Slesa
+ * @version 1.01.2025
+ */
 
+// this Main class - это класс Маин
 public class Main {
+    // this method "main" is point to our program - этот метод маин запускает программу
+
+    public static void main(String[] args) {
+
+        /*
+        this instruction - эта инструкция
+        print string - выводит строку
+        with greeting
+         */
+
+        System.out.println("My name is Alexandr!");
+
+
+
+
+
+
+    }
 }
